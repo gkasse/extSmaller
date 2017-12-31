29802119efe95aa3f012a474769d7f47297e75e7
gkasse <horizontal.genki3317@gmail.com>
2017-12-31 22:00:50 +0900

    Leave the God of your mind

--- internal/resolver/filepathResolver.go
+++ internal/resolver/filepathResolver.go
@@ -12,8 +12,11 @@ const (
 	userHomeDirectoryAlias = "~"
 )
 
+// FilepathResolver is resolving full path
 type FilepathResolver struct{}
 
+// Resolve is convert to absolute path
+// e.g.) '~' convert to user's home directory
 func (r FilepathResolver) Resolve(path string) string {
 	currentUser, e := user.Current()
 	if e != nil {

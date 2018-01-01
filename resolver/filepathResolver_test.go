b59753ee63329fe98259412a31f21048e5c02faf
gkasse <horizontal.genki3317@gmail.com>
2018-01-02 01:13:12 +0900

    Change how to get home directory in test

--- resolver/filepathResolver_test.go
+++ resolver/filepathResolver_test.go
@@ -2,6 +2,8 @@ package resolver
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -12,16 +14,17 @@ func TestFilepathResolver_Resolve(t *testing.T) {
 	}
 	tests := []struct {
 		name string
-		r    FilepathResolver
 		args args
-		want string
+		want func() string
 	}{
-		{"contains home directory alias", FilepathResolver{}, args{"~/Download/test"}, "/Users/seinflaw/Download/test"},
+		{"contains home directory alias", args{"~/Download/test"}, func() string {
+			return filepath.Join(os.Getenv("HOME"), "Download/test")
+		}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := FilepathResolver{}
-			if got := r.Resolve(tt.args.path); got != tt.want {
+			if got := r.Resolve(tt.args.path); got != tt.want() {
 				t.Errorf("FilepathResolver.Resolve() = %v, want %v", got, tt.want)
 			}
 		})
